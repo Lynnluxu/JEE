b38116a69e047a42ad6010a7f10ac19e49889736
Lynnluxu <luxu_lynn@hotmail.com>
2020-01-28 00:00:00 +0100

    [lab04-web]implement Initializer

---
+++ lab04-web/src/main/java/yncrea/lab04/web/Initializer.java
@@ -0,0 +1,23 @@
+package yncrea.lab04.web;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import yncrea.lab04.core.config.AppConfig;
+import yncrea.lab04.core.config.DBConfig;
+
+public class Initializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{AppConfig.class, DBConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
+
